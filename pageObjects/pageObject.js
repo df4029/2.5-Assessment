eff1c6fb845c6b1bd0da3cc1753860cb54dc6b8a
Daniel Flanagan <df4029@gmail.com>
2018-07-16 11:50:37 -0600

    DF 2.5automatedtestfinal

---
+++ pageObjects/pageObject.js
@@ -0,0 +1,28 @@
+module.exports = {
+    url: 'http://localhost:3000/',
+    elements: {
+        //pageobjects elements
+        "evenOddIn": 'input[name="evenOddInput"]',
+        "evenOddBut":'button[name="evenOddButton"]',
+        "evenResults":'span[name="evenResults"]',
+        "oddResults":'span[name="oddResults"]',
+        "obFilterOb":'span[name="objectFilterObjects"]',
+        "obFilterIn":'input[name="objectFilterInput"]',
+        "obFilterBut":'button[name="objectFilterButton"]',
+        "obFilterRes":'span[name="objectFilterResults"]',
+        "namFilterNam":'span[name="nameFilterNames"]',
+        "namFilterIn":'input[id="nameFilterInput"]',
+        "namFilterBut":'button[id="nameFilterButton"]',
+        "namFilterRes":'span[name="nameFilterResults"]',
+        "palinInput":'input[name="palindromeInput"]',
+        "palinButt":'button[name="palindromeButton"]',
+        "palinRes":'span[name="palindromeResults"]',
+        "sumIn1":'input[name="sumInput1"]',
+        "sumIn2":'input[name="sumInput2"]',
+        "sumBut":'button[name="sumButton"]',
+        "sumRes":'span[name="sumResults"]',
+    
+    }
+
+
+}
